7dafdb6be2b83d36b7e8ebaaef293205283329ae
Enver Ismailov <113299648+Enver074@users.noreply.github.com>
2022-10-20 13:38:22 +0300

    Первое задание

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+// 0, 7, 8, -2, -2 -> 2
+
+// 1, -7, 567, 89, 223-> 4
+
+Console.WriteLine("Введите несколько чисел: ");
+string num = (Console.ReadLine()!);
+
+Console.WriteLine($"Количество чисел больше 0 равно: {GetCount(num)}");
+
+
+int GetCount(string number){
+string[] array = number.Split(',');
+int count = 0;
+foreach (var item in array)
+{
+   int i = int.Parse(item);
+   if(i > 0)
+        count++;
+}
+return count;
+}
